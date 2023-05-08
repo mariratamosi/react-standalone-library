d2b250abd3bc942a7ca6674e16d9f25a1c165cb2
DESKTOP-8V3V3US\REMA <rongrazzo@gmail.com>
2023-05-08 18:30:00 +0600

    working!@

---
+++ src/Addition.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Addition() {
+  return (
+    <div className="app-container-2">
+        <h3>This ones workingc</h3>
+    </div>
+  );
+}
+
+export default Addition;
